02d34cdd3b23faf5f6fb0a616095db86c76c17d7
Amadeus Folego <amadeusfolego@gmail.com>
2018-02-19 18:40:57 -0300

    Add usage, urgency and keep output options

--- README.md
+++ README.md
@@ -3,20 +3,41 @@ Grep-Notify
 
 Reads from stdout and `notify-send` lines matched via regex
 
-Example:
+## Usage
+
+```
+Usage: ./grep-notify [-k] [-u URGENCY] [PATTERN] [TITLE]
+
+DESCRIPTION
+  grep-notify searches for PATTERN from STDIN and sends a notification
+  whenever it's matched.  If no TITLE is given, the notification title
+  is 'Grep Notify'.
+
+OPTIONS
+  -k Keep the original program STDOUT
+  -u Set the urgency to low|normal|critical. Default: normal.
+```
+
+## Examples
 
     $ cat file
     I am a line
     I am a line too!
     I am the line two!
-    
+
     $ cat file | grep-notify too
+    I am a line too!
     # => 'I am a line too!' is sent
-    
+
     $ cat file | grep-notify t.o
+    I am a line too!
+    I am the line two!
     # => 'I am a line too!'   is sent
     # => 'I am the line two!' is sent
 
-    # You can also choose the title of the notification
-    $ cat file | grep-notify too 'Grepped Program'
-    # => 'I am a line too!' is sent with 'Grepped Program' title
+    $ cat file | grep-notify -k t.o
+    I am a line
+    I am a line too!
+    I am the line two!
+    # => 'I am a line too!'   is sent
+    # => 'I am the line two!' is sent
--- grep-notify.c
+++ grep-notify.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+#include <string.h>
 #include <stdlib.h>
 #include <regex.h>
 #include <stdio.h>
@@ -5,37 +7,84 @@
 
 char *program_name;
 
-void notify(char * message) {
+void notify(char * message, NotifyUrgency urgency) {
 	NotifyNotification* notification = notify_notification_new(program_name, message, "dialog-information");
+	notify_notification_set_urgency(notification, urgency);
 	notify_notification_show(notification, NULL);
 	g_object_unref(G_OBJECT(notification));
 }
 
+void print_usage(char * program) {
+	printf("Usage: %s [-k] [-u URGENCY] [PATTERN] [TITLE]\n\n", program);
+	puts("DESCRIPTION");
+	puts("  grep-notify searches for PATTERN from STDIN and sends a notification");
+	puts("  whenever it's matched.  If no TITLE is given, the notification title");
+	puts("  is 'Grep Notify'.\n");
+	puts("OPTIONS");
+	puts("  -k Keep the original program STDOUT");
+	puts("  -u Set the urgency to low|normal|critical. Default: normal.");
+}
+
 int main(int argc, char **argv) {
+	NotifyUrgency urgency = NOTIFY_URGENCY_NORMAL;
+	int keep_stdout = false;
+	size_t optind;
+	for (optind = 1; optind < argc && argv[optind][0] == '-'; optind++) {
+		switch (argv[optind][1]) {
+		case 'k': keep_stdout = true; break;
+		case 'u':
+			optind++;
+			if (strcmp(argv[optind], "low") == 0) {
+				urgency = NOTIFY_URGENCY_LOW;
+			}
+			else if (strcmp(argv[optind], "normal") == 0) {
+				urgency = NOTIFY_URGENCY_NORMAL;
+			}
+			else if (strcmp(argv[optind], "critical") == 0) {
+				urgency = NOTIFY_URGENCY_CRITICAL;
+			}
+			else {
+				fprintf(stderr, "Invalid urgency setting, valid settings: low|normal|critical\n\n");
+				print_usage(argv[0]);
+				return 1;
+			}
+			break;
+		default:
+			print_usage(argv[0]);
+			return 1;
+		}
+	}
+
 	regex_t regex;
 	char *line = NULL;
 	size_t size = 0;
 	int reti;
 
-	if (argc < 2) {
-		fprintf(stderr, "At least one argument must be provided\n");
+	if (!argv[optind]) {
+		fprintf(stderr, "At least one argument must be provided\n\n");
+		print_usage(argv[0]);
 		return 1;
 	}
 
-	reti = regcomp(&regex, argv[1], 0);
+	reti = regcomp(&regex, argv[optind], 0);
 	if (reti) {
 		fprintf(stderr, "Could not compile regex\n");
 		return 1;
 	}
 
-	program_name = (argc > 2) ? argv[2] : "Grep Notify";
+	program_name = (argv[optind+1]) ? argv[optind+1] : "Grep Notify";
 
 	notify_init(program_name);
 
 	while (getline(&line, &size, stdin) != -1) {
 		if (!regexec(&regex, line, 0, NULL, 0)) {
-			fprintf(stdout, line);
-			notify(line);
+			if (!keep_stdout) {
+				printf(line);
+			}
+			notify(line, urgency);
+		}
+		if (keep_stdout) {
+			printf(line);
 		}
 	}
 
